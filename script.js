8e797b921ccbf1d9e2d88272e3b27044cfb156b2
Monik <monikcostaal@gmail.com>
2024-03-14 19:57:21 -0300

    :teste:  initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function toggleMode(){
+  const html = document.documentElement;
+
+  html.classList.toggle("light");
+
+  const img = document.querySelector("#profile img")
+
+  if(html.classList.contains('light')){
+    img.setAttribute("src", 'assets/avatar.png')
+  }else{
+    img.setAttribute("src", "assets/avatar-light.png")
+  }
+}
+
+
+// function toggleMode() {
+//   const html = document.documentElement
+//    if(html.classList.contains('light')){
+//     html.classList.remove("light")
+//    }else{
+//      html.classList.add(`light`)
+//    }
+// }
