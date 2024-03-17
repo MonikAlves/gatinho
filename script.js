c11e21ffa889fdb664c8b8edf2b293bf59a527eb
Monik <monikcostaal@gmail.com>
2024-03-17 19:10:19 -0300

    atualizando url

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ function toggleMode(){
   const img = document.querySelector("#profile img")
 
   if(html.classList.contains('light')){
-    img.setAttribute("src", 'assets/avatar.png')
+    img.setAttribute("src", './assets/avatar.png')
   }else{
-    img.setAttribute("src", "assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatar-light.png")
   }
 }
 
